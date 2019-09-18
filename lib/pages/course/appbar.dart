3fd7810736b05e6c838d7b382620f901bdd197ba
Saurabh Petkar <saurabhpetkar@gmail.com>
2019-09-19 00:47:01 +0530

    home page, about, share.

---
+++ lib/pages/course/appbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class appbar extends StatelessWidget with PreferredSizeWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey;
+  appbar(this._scaffoldKey);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('WealthSuite'),
+      backgroundColor: Color(0xFF18D191),
+      elevation: 2.0,
+      leading: IconButton(
+        icon: Transform(
+            transform: Matrix4.skewX(-0.3),
+            child: Icon(
+              Icons.list,
+              size: 25,
+            )),
+        onPressed: () {
+          _scaffoldKey.currentState.openDrawer();
+        },
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
